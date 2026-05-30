1f205b85737442830a0757bb009363cb41a40b70
stagas <gstagas@gmail.com>
2026-05-30 18:34:02 +0300

    outside start time

--- src/scene-data.ts
+++ src/scene-data.ts
@@ -86,7 +86,7 @@ export const videoTracks: Record<VideoZone, string> = {
 }
 export const videoStartTimes: Record<VideoZone, number> = {
   inside: 0,
-  outside: 10,
+  outside: 25,
   tent: 0,
 }
 export const videoPlaylists: Partial<Record<VideoZone, string>> = {
